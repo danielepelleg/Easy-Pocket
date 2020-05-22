aa2812231201cd04eef1fd78ca80719706230510
danielepelleg <55688016+danielepelleg@users.noreply.github.com>
2020-05-22 02:13:53 +0200

    Add SignOut Button for User Authentication

---
+++ src/components/SignOut.js
@@ -0,0 +1,17 @@
+import React from 'react';
+ 
+/// Firebase
+import { withFirebase } from './Firebase';
+
+
+/**
+ *    *** SIGN OUT BUTTON ***
+ * Sign Out Button to sign the user out.
+ */
+const SignOutButton = ({ firebase }) => (
+  <button type="button" onClick={firebase.doSignOut}>
+    Sign Out
+  </button>
+);
+ 
+export default withFirebase(SignOutButton);
