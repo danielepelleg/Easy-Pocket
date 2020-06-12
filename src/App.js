50e0000bcaa305c37041a45f50afff4af0434da0
gweedo <guido.s1998@gmail.com>
2020-06-12 09:22:57 +0200

    Push problem

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import Home from './Home.js'
-import Login from './components/Login.js'
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-        
-      </div>
-    </Router>
-  );
-=======
 import {Router} from 'react-router-dom';
 import {createBrowserHistory} from 'history';
 import {Chart} from 'react-chartjs-2';
 import {ThemeProvider} from '@material-ui/styles';
 import validate from 'validate.js';
-
-
 import {chartjs} from './helpers';
 import theme from './theme';
 import 'react-perfect-scrollbar/dist/css/styles.css';
@@ -57,7 +36,7 @@ function App() {
             </Router>
         </ThemeProvider>
     );
->>>>>>> Stashed changes
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
-<<<<<<< Updated upstream
 import * as serviceWorker from './serviceWorker';
-=======
-import Firebase, { FirebaseContext } from './Firebase';
->>>>>>> Stashed changes
+
 
 ReactDOM.render(
   <React.StrictMode>
