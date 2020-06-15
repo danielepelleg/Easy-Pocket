22dc6ea03777e1c3af053aa16b92753d8969b200
danielepelleg <55688016+danielepelleg@users.noreply.github.com>
2020-06-15 16:01:27 +0200

    Add SignOutButton in Components folder

---
+++ src/elements/SignOut/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import InputIcon from "@material-ui/icons/Input";
+import {IconButton} from '@material-ui/core';
+
+/// Firebase
+import { withFirebase } from '../../components/Firebase';
+
+/**
+ *    *** SIGN OUT BUTTON ***
+ * Sign Out Button to sign the user out.
+ */
+const SignOutButton = ({ firebase}) => (
+  <IconButton
+    color="inherit"
+    onClick={firebase.doSignOut} >
+    <InputIcon />
+  </IconButton>
+);
+
+export default withFirebase(SignOutButton);
