90c922e8c130ddd183e8da5b41153b2116f89cef
danielepelleg <55688016+danielepelleg@users.noreply.github.com>
2020-09-03 00:40:09 +0200

    Delete Card Profile and Card Details components from Cards

--- src/components/Cards/components/index.js
+++ src/components/Cards/components/index.js
@@ -1,5 +1,3 @@
-export { default as CardDetails } from './CardDetails';
-export { default as CardProfile } from './CardProfile';
 export { default as AddCard } from './AddCard';
 export { default as CardItem } from './CardItem';
 export { default as CardList } from './CardList';
