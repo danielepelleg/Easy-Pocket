1c26dd7d042687bc0c69ccbbb0235352e83deb58
Daniele Pellegrini <daniele.pellegrini@studenti.unipr.it>
2020-09-09 17:19:04 +0200

    Delete PurchaseItem export

--- src/components/Purchases/components/index.js
+++ src/components/Purchases/components/index.js
@@ -1,5 +1,4 @@
 export { default as CardDetails } from './CardDetails';
 export { default as CardProfile } from './CardProfile';
 export { default as AddPurchase } from './AddPurchase';
-export { default as PurchaseItem } from './PurchaseItem';
 export { default as PurchaseList } from './PurchaseList';
