9b3eb75b2853a7d67e5c8d8a819d2494d6bd29a7
Daniele Pellegrini <daniele.pellegrini@studenti.unipr.it>
2020-09-11 01:47:13 +0200

    Delete CardProfile and CardDetails Components
    These components has been used as a structure to create the other components with forms and informations.

--- src/components/Purchases/components/index.js
+++ src/components/Purchases/components/index.js
@@ -1,4 +1,2 @@
-export { default as CardDetails } from './CardDetails';
-export { default as CardProfile } from './CardProfile';
 export { default as AddPurchase } from './AddPurchase';
 export { default as PurchaseList } from './PurchaseList';
