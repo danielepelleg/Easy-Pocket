de8d49b178594935683aadcdffc2a36872714161
Daniele Pellegrini <daniele.pellegrini@studenti.unipr.it>
2020-09-26 16:39:58 +0200

    Delete unused exports

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@
  * Dashboard Exports
  */
 export { default as SearchInput } from './SearchInput';
-export { default as StatusBullet } from './StatusBullet';
 export { default as RouteWithLayout } from './RouteWithLayout';
 
 /**
