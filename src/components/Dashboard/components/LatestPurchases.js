f6580ef9bcdaf04a791311ed19310b268094c305
Daniele Pellegrini <daniele.pellegrini@studenti.unipr.it>
2020-09-26 16:01:21 +0200

    Delete unused imports

--- src/components/Dashboard/components/LatestPurchases.js
+++ src/components/Dashboard/components/LatestPurchases.js
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 import { Bar } from "react-chartjs-2";
 import {
   Box,
-  Button,
   Card,
   CardContent,
   CardHeader,
@@ -13,7 +12,6 @@ import {
   makeStyles,
   colors,
 } from "@material-ui/core";
-import ArrowRightIcon from "@material-ui/icons/ArrowRight";
 
 const useStyles = makeStyles(() => ({
   root: {},
