d9bcf2bd606c34c57d20b98b02cee5a8b5b7ae78
Daniele Pellegrini <daniele.pellegrini@studenti.unipr.it>
2020-09-29 00:34:58 +0200

    Complete Credits View

--- src/views/Credits/Credits.js
+++ src/views/Credits/Credits.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import theme from "../../theme";
-import {Grid} from "@material-ui/core";
+import {Grid, Card, CardContent} from "@material-ui/core";
 import CreditsCard from "./CreditsCards";
 import Typography from "@material-ui/core/Typography";
 
@@ -17,50 +17,50 @@ class CreditsBase extends Component {
         const authors = [
                 {
                     name: "Daniele Pellegrini",
-                    image:"https://picsum.photos/200/300",
+                    image:"https://www.flaticon.com/svg/static/icons/svg/616/616451.svg",
                     github: "https://github.com/danielepelleg",
-                    email: "dannonsolamail@gmail.com",
-                    description: "i'm a good guy"
+                    email: "daniele.pellegrini@studenti.unipr.it",
+                    color: "#068FE3"
                 },
                 {
                     name: "Guido Soncini",
-                    image:"https://picsum.photos/200/300",
+                    image:"https://www.flaticon.com/svg/static/icons/svg/1970/1970703.svg",
                     github: "https://github.com/gweedo",
-                    email: "guido.s1998@gmail.com",
-                    description: "lol"
+                    email: "guido.soncini1@studenti.unipr.it",
+                    color: "#208B10"
                 }
         ]
-        const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed massa ante. Etiam suscipit nulla a ligula fermentum, sed feugiat nulla sollicitudin. Donec vestibulum, massa at sagittis placerat, magna purus dictum orci, quis sodales velit ante eu tellus. Duis quis est odio. Pellentesque venenatis a ipsum ac mattis. Nam sit amet ligula vel libero malesuada tempus ut id lacus. Quisque urna arcu, maximus ac dignissim at, egestas ac quam. Curabitur sed dictum lectus. Ut magna dolor, semper et diam eu, placerat commodo nibh. Nam vitae rutrum est, ut porta est. Duis cursus at elit vitae tempor."
+
         return (
             <div className={this.props.classes.root}>
                 <Grid container spacing={3}>
-                    <Grid
-                        item
-                        lg={2}
-                        md={6}
-                        xl={4}
-                        xs={12}
-                    />
                     { authors.map((_author) =>{
                         return(
                             <Grid
                                 item
-                                lg={4}
+                                lg={6}
                                 md={6}
-                                xl={3}
+                                xl={6}
                                 xs={12}
                             >
-                                <CreditsCard name = {_author.name} github={_author.github} email = {_author.email} image={_author.image} description = {_author.description}/>
+                                <CreditsCard name = {_author.name} github={_author.github} email = {_author.email} image={_author.image} color = {_author.color}/>
                             </Grid>
                         );
                     })}
                     <Grid
                         item
-                        lg={9}
-                        md={6}
-                        xl={9}
+                        lg={12}
+                        md={12}
+                        xl={12}
                         xs={12}>
-                        <Typography ></Typography>
+                            <Card style={{ opacity: 0.7 }}>
+                                <CardContent>
+                        <Typography variant="h3">
+                        This web application has been created for the course "Internet Technologies" at University of Parma. <br></br>
+                        The site has been coded using React native. It allows to store user's datas such as credit cards and payments without take the more private data or the sensitives ones.
+                        </Typography>
+                        </CardContent>
+                        </Card>
                     </Grid>
 
 
--- src/views/Credits/CreditsCards/CreditsCard.js
+++ src/views/Credits/CreditsCards/CreditsCard.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Card} from "@material-ui/core";
+import {Card, CardHeader} from "@material-ui/core";
 import CardContent from "@material-ui/core/CardContent";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
@@ -21,7 +21,9 @@ const useStyles = makeStyles({
         color:"red"
     },
     subtitle:{
-        marginTop: theme.spacing(1),
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+        fontSize: "24px",
         color:"orange"
     }
 
@@ -31,17 +33,18 @@ class CreditsCardBase extends Component {
 
 render() {
         return(
-            <Card className={this.props.classes.root} variant="outlined">
+            <Card className={this.props.classes.root} variant="outlined" style = {{backgroundColor: this.props.color, opacity: 0.85}}>
+                <Avatar alt="Photo" src={this.props.image} className={this.props.classes.avatar} />
+                <CardHeader
+                    titleTypographyProps={{ component: "h1", variant: "h1" }}
+                    title= {this.props.name}
+                />
                 <CardContent>
-                    <Avatar alt="Photo" src={this.props.image} className={this.props.classes.avatar} />
-                    <Typography variant="h4" component="h3" className={this.props.classes.title}> {this.props.name} </Typography>
                     <Typography className={this.props.classes.subtitle}>GitHub</Typography>
-                    <Typography component="p"> {this.props.github} </Typography>
+                    <Typography variant="h4"> <a href={this.props.github}> {this.props.github} </a></Typography>
                     <Typography className={this.props.classes.subtitle}>Email</Typography>
-                    <Typography component="p"> {this.props.email} </Typography>
-                    <Typography className={this.props.classes.subtitle}>Description</Typography>
+                    <Typography variant="h4"> {this.props.email} </Typography>
                     {/*maybe create a collapse card with a long and nice description*/}
-                    <Typography component="p"> {this.props.description}</Typography>
                 </CardContent>
             </Card>
         )
