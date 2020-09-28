c8ea02e7a2d294ad38198a8685273039a51ac05f
Daniele Pellegrini <daniele.pellegrini@studenti.unipr.it>
2020-09-28 16:02:00 +0200

    Delete non used imports in Footer

--- src/layouts/Main/components/Footer/Footer.js
+++ src/layouts/Main/components/Footer/Footer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
-import { Typography/*, Link*/ } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,8 +19,6 @@ const Footer = props => {
       {...rest}
       className={clsx(classes.root, className)}
     >
-      <Typography variant="body1">
-      </Typography>
     </div>
   );
 };
